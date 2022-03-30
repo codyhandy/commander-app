be36dd777928e4d72d6d019060a14b2e8729c95d
Cody <55256070+codyhandy@users.noreply.github.com>
2022-03-29 18:11:24 -0600

    Started project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # commander-tracker
- A web app that tracks data for commander games in my playgroup
+ A web app that tracks data for commander games in my playgroup. Created for fun and a way to practice noSQL databases, JSON, and React.
---
+++ commander-tracker/pages/index.js
@@ -0,0 +1,61 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Commander Tracker</h1>
+
+      <button className="new-game-btn">
+        Start New Game
+      </button>
+
+      <section className="recent-games">
+        <h2>Recently Played Games</h2>
+        <div className="game">
+          <div className="players">
+
+            <div className="indv-player">
+              <p>Cody</p>
+              <div className="cid">
+                <Image src="/img/R.png" alt='Red Mana Symbol' width={18} height={18} />
+                <Image src="/img/G.png" alt='Green Mana Symbol' width={18} height={18} />
+                <Image src="/img/W.png" alt='White Mana Symbol' width={18} height={18} />
+              </div>
+            </div>
+
+            <div className="indv-player">
+              <p>Riley</p>
+              <div className="cid">
+                <Image src="/img/G.png" alt='Green Mana Symbol' width={18} height={18} />
+                <Image src="/img/W.png" alt='White Mana Symbol' width={18} height={18} />
+              </div>
+            </div>
+
+            <div className="indv-player">
+              <p>Igor</p>
+              <div className="cid">
+                <Image src="/img/U.png" alt='Blue Mana Symbol' width={18} height={18} />
+                <Image src="/img/B.png" alt='Black Mana Symbol' width={18} height={18} />
+                <Image src="/img/R.png" alt='Red Mana Symbol' width={18} height={18} />
+              </div>
+            </div>
+
+            <div className="indv-player">
+              <p>Zack</p>
+              <div className="cid">
+                <Image src="/img/U.png" alt='Blue Mana Symbol' width={18} height={18} />
+                <Image src="/img/B.png" alt='Black Mana Symbol' width={18} height={18} />
+                <Image src="/img/R.png" alt='Red Mana Symbol' width={18} height={18} />
+              </div>
+            </div>
+
+          </div>
+          {/* end of the players div */}
+          <p className='timestamp'>5 days ago</p>
+        </div>
+      </section>
+    </>
+    
+  )
+}
